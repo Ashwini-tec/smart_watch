a2936bfe30a87e0744840185c381e6e7d2939987
Ashwini-tec <ashiwni@ariantechsolutions.com>
2022-05-31 11:02:09 +0530

    add server url to swagger docs

--- middleware/patientSchema.js
+++ middleware/patientSchema.js
@@ -42,6 +42,22 @@ module.exports = {
             citizenship: Joi.string(),
             patientType: Joi.string().valid( "patient", "surrogate", "donor" ),
             cycleType: Joi.string().valid( "self-IVF", "egg-donor", "surrogate", "IUI" ),
+            metaData: Joi.object({
+                name: Joi.string().required(),
+                DOB: Joi.string().required(),
+                bloodGroup: Joi.string().required(),
+                gender: Joi.string().valid( "male", "female", "other" ).required(),
+                picture: Joi.string().required(),
+                identifyProof: Joi.string().required(),
+                citizenship: Joi.string().required(),
+            }).when(
+                "patientType",
+                {
+                    is: "patient",
+                    then: Joi.required(),
+                    otherwise: Joi.optional(),
+                }
+            )
         });
     },
 };
